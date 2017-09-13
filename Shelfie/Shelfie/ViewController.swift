32b6667b85ba2209eff21c47cb2c409142f86c5e
iosdev <iosdev@mtk-idev-mba-30.local>
2017-09-13 14:50:01 +0300

    fresh project

---
+++ Shelfie/Shelfie/ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  Shelfie
+//
+//  Created by iosdev on 13.9.2017.
+//  Copyright © 2017 Group-6. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func makeStuff(){
+        print("making stuff");
+        
+    }
+
+
+}
+
