df5eb2285b8302fb80b27f1d10252cf825b6fc35
iosdev <iosdev@mtk-idev-mba-30.local>
2017-10-03 01:08:15 +0300

    made resizing boxes incremental

--- Shelfie/Shelfie/BoxViewController.swift
+++ Shelfie/Shelfie/BoxViewController.swift
@@ -11,7 +11,9 @@ import UIKit
 class BoxViewController: UIViewController {
 
     var newestView: UIView?;
-    
+    var currentTotalX: CGFloat = 0;
+    var currentTotalY: CGFloat = 0;
+    let increment: CGFloat = 50.0;
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -44,7 +46,7 @@ class BoxViewController: UIViewController {
         case .began:
             
             let coord = sender.location(in: sender.view);
-            print("\(coord.x) : \(coord.y)");
+            //print("\(coord.x) : \(coord.y)");
             //create new view
             let newView=UIView(frame: CGRect(x: coord.x, y: coord.y, width: 0, height: 0));
             newView.backgroundColor = UIColor.blue;
@@ -54,7 +56,30 @@ class BoxViewController: UIViewController {
         case .changed:
             let translation = sender.translation(in: self.view)
             if newestView != nil {
-                newestView!.frame = CGRect(x: newestView!.frame.origin.x, y: newestView!.frame.origin.y, width: newestView!.frame.width + translation.x, height: newestView!.frame.height + translation.y);
+                currentTotalX += translation.x;
+                currentTotalY += translation.y;
+                if(abs(currentTotalX) > increment && newestView!.frame.size.width + currentTotalX > 0){
+                    newestView!.frame.size.width += roundToNearest(x: currentTotalX);
+                    currentTotalX = 0;
+                }
+                if(abs(currentTotalY) > increment && newestView!.frame.size.height + currentTotalY > 0){
+                    newestView!.frame.size.height += roundToNearest(x: currentTotalY);
+                    currentTotalY = 0;
+                }
+                /*
+                let newWidth = newestView!.frame.size.width+translation.x;
+                let newHeight = newestView!.frame.size.height + translation.y;
+                print("NewWidth \(newWidth) NewHeight \(newHeight)");
+                if(newWidth > 0.5){
+                    newestView!.frame.size.width=roundToNearest(x:newWidth);
+                }
+                if(newHeight > 0.5){
+                    
+                    newestView!.frame.size.height = roundToNearest(x: newHeight);
+                }
+                print("NewWidth2 \(newestView!.frame.size.width) NewHeight2 \(newestView!.frame.size.height)");*/
+                //print("\(newestView!.frame.origin.x) : \(newestView!.frame.origin.y)");
+                //newestView!.frame = CGRect(x: newestView!.frame.origin.x, y: newestView!.frame.origin.y, width: newestView!.frame.width + translation.x, height: newestView!.frame.height + translation.y);
             }
             break;
         case .ended:
@@ -71,6 +96,12 @@ class BoxViewController: UIViewController {
             
         }
         sender.setTranslation(CGPoint.zero, in: self.view)
+    }
+    
+    func roundToNearest(x : CGFloat) -> CGFloat {
+        let jee = increment * CGFloat(round(x / increment));
+        print("Rounded \(x) to \(jee)");
+        return jee;
     }
 
 }
